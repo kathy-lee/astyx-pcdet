f5da6bca3e5a80008aeaff71b611cd150f993168
kathy <kangle.leehd@gmail.com>
2020-12-04 00:09:24 +0100

    modified return value

--- pcdet/models/detectors/pointnet_seg.py
+++ pcdet/models/detectors/pointnet_seg.py
@@ -18,8 +18,7 @@ class PointNetSeg(Detector3DTemplate):
             }
             return ret_dict, tb_dict, disp_dict
         else:
-            pred_dicts, recall_dicts = self.post_processing(batch_dict)
-            return pred_dicts, recall_dicts
+            return batch_dict
 
     def get_training_loss(self):
         disp_dict = {}
